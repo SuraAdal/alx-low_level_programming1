084c8622fac4d2ac05662ae6e8a071301f2955e3
SuraAdal <133393569+SuraAdal@users.noreply.github.com>
2023-06-16 00:26:41 +0300

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,7 @@
-#include <stdio.h>
-#include <unistd.h>
+#include<stdio.h>
 
-/**
-* main - Entry point
-*
-* Return: Always 1 (Success)
-*/
 int main(void)
 {
-        write(2,
-        "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-        return (1);
+    puts("Programming is like building a multilingual puzzle\n");
+    return 0;
 }
