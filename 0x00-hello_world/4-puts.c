505c910220ab71ddaa26fcf1eedf0681cd801ff1
SuraAdal <vmos2020@yahoo.com>
2023-06-15 15:05:57 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,8 +2,9 @@
 /**
  *main - entry point
  *return : always 0 (sucess)
- */int main(void)
+ */
+int main(void)
 {
-    puts("Programming is like building a multilingual puzzle");
-    return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
