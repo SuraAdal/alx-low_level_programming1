b30a91683b114c034179f0eced9a2bd59ba85156
SuraAdal <vmos2020@yahoo.com>
2023-06-15 15:25:06 -0700

    main

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 /**
  *main - entry point
- *return : always 0 (sucess)
+ *
+ * return : always 0 (sucess)
  */
 void main(void)
-int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
