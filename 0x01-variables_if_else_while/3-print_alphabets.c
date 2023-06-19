95374b71376b1789a1d9015489580d038f4a1e39
SuraAdal <vmos2020@yahoo.com>
2023-06-19 02:29:32 -0700

    final commit

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -6,11 +6,14 @@
  */
 int main(void)
 {
-	char letter;
-
-
-	for (letter = 'a'; letter <= 'z'; letter++)
-		putchar(letter);
+	char lowercase;
+	char uppercase;
+	
+	
+	for (lowercase = 'a'; lowercase <= 'z'; lowercase++)
+		putchar(lowercase);
+	for (uppercase = 'A'; uppercase <= 'Z'; uppercase++)
+		putchar(uppercase);
 	putchar('\n');
-	return (0);
+	return (0);	
 }
