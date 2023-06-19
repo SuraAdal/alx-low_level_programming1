41e14a18814ad02ffd9834988fc05fb655413855
SuraAdal <vmos2020@yahoo.com>
2023-06-19 02:20:38 -0700

    alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ *main - Entry point of the program
+ *Discription:'list out alphabets'
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char letter;
+
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
+	putchar('\n');
+	return (0);
+}
