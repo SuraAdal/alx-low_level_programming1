0a31c56e63c698e00431cdc53e5193ad5bba4af5
SuraAdal <vmos2020@yahoo.com>
2023-06-20 16:00:18 -0700

    final

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,22 +1,20 @@
-#include <main.h>
+#include "main.h"
 /**
-* print_alphabets - Prints the alphabet in lowercases'
-* Return: Always 0 (Success)
-*/
+ * main - Entry point
+ * Discription: 'prints alphabets in lower case
+ * print_alphabets - Prints the alphabet in lowercases'
+ * Return: Always 0 (Success)
+ * */
 void print_alphabet(void)
 {
 	char c;
-	
+
 	for (c = 'a'; c <= 'z'; c++)
 	{
-		putchar(c);
+		_putchar(c);
 	}
 	putchar('\n');
 }
-/**
-* print_alphabets - Prints the alphabet in lowercases'
-* Return: Always 0 (Success)
-*/
 int main(void)
 {
 	print_alphabet();
